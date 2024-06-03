f9c8ec2f84813b16eb82f929558eaad5740c7bd0
Murugan.V <murugan.v@in.artofliving.org>
2024-06-03 17:45:39 +0530

    newcommit

--- pages/register.jsx
+++ pages/register.jsx
@@ -17,7 +17,7 @@ export default function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Send form data to API endpoint (to be created)
-    const response = await fetch('https://worker-nameless-wildflower-3a34.kkdgantester.workers.dev/', {
+    const response = await fetch('https://74626f96.register-backend.pages.dev', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
